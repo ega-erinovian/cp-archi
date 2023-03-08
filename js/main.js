31bca6f8320288ae4bb8f252db2d710414d375fa
ega-erinovian <erinovian69@gmail.com>
2023-03-09 01:05:39 +0700

    rampung ges

--- js/main.js
+++ js/main.js
@@ -63,16 +63,10 @@
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
             $('.tombol-wa').fadeIn('slow');
         } else {
-            $('.back-to-top').fadeOut('slow');
             $('.tombol-wa').fadeOut('slow');
         }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
     });
 
 
