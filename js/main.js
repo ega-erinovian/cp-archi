5ac70f1f8310501df911c913cec2ea37e6c0b0d7
ega-erinovian <erinovian69@gmail.com>
2023-03-08 22:11:24 +0700

    fixing bidang, sertifikat, maps

--- js/main.js
+++ js/main.js
@@ -123,6 +123,19 @@
             }
         }
     });
+
+
     
 })(jQuery);
 
+let body = document.querySelector("body");
+let main = document.querySelector(".main");
+body.addEventListener("click", function (e) {
+  if (e.target.classList[0] == "airimage") {
+    main.classList.add("main2");
+    main.innerHTML = `<img class="boximage" src="${e.target.dataset.url}" alt="one"  /> <i class="bi bi-x close"></i>`;
+  } else {
+    main.classList.remove("main2");
+    main.innerHTML = "";
+  }
+});
